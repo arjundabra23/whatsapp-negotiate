edf262e855c010d768f41185f6f1064fa10fac2c
Abdullah Nauman <abdullahnauman@google.com>
2024-05-18 16:59:03 -0700

    added for taking to each other

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express().use(body_parser.json());
 const token = process.env.TOKEN;
 const mytoken = process.env.MYTOKEN;//prasath_token
 
+
+RESTAURANT_OWNER = "+15556066529";
+VENDOR_1 = "+16315900900";
+
 app.listen(process.env.PORT, () => {
     console.log("webhook is listening");
 });
@@ -38,36 +42,61 @@ app.post("/webhook", (req, res) => { //i want some
     console.log(JSON.stringify(body_param, null, 2));
 
     if (body_param.object) {
+        // we recieve a message
         console.log("inside body param");
         if (body_param.entry &&
             body_param.entry[0].changes &&
             body_param.entry[0].changes[0].value.messages &&
             body_param.entry[0].changes[0].value.messages[0]
         ) {
-            let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = body_param.entry[0].changes[0].value.messages[0].from;
-            let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+            let sender_num = body_param.entry[0].changes[0].value.messages[0].from;
+            if (sender_num == RESTAURANT_OWNER) {
+                let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-            console.log("phone number " + phon_no_id);
-            console.log("from " + from);
-            console.log("boady param " + msg_body);
+                console.log("phone number " + phon_no_id);
+                console.log("from " + from);
+                console.log("boady param " + msg_body);
 
-            axios({
-                method: "POST",
-                url: "https://graph.facebook.com/v13.0/" + phon_no_id + "/messages?access_token=" + token,
-                data: {
-                    messaging_product: "whatsapp",
-                    to: "+16315900900",
-                    text: {
-                        body: "Hi.. I'm Prasath, your message is " + msg_body
+                axios({
+                    method: "POST",
+                    url: "https://graph.facebook.com/v13.0/" + phon_no_id + "/messages?access_token=" + token,
+                    data: {
+                        messaging_product: "whatsapp",
+                        to: VENDOR_1,
+                        text: {
+                            body: "Hi.. I'm OWNER, your forwarded message is " + msg_body
+                        }
+                    },
+                    headers: {
+                        "Content-Type": "application/json"
                     }
-                },
-                headers: {
-                    "Content-Type": "application/json"
-                }
 
-            });
+                });
+            } else if (sender_num == VENDOR_1) {
+                let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
+                console.log("phone number " + phon_no_id);
+                console.log("from " + from);
+                console.log("boady param " + msg_body);
+
+                axios({
+                    method: "POST",
+                    url: "https://graph.facebook.com/v13.0/" + phon_no_id + "/messages?access_token=" + token,
+                    data: {
+                        messaging_product: "whatsapp",
+                        to: RESTAURANT_OWNER,
+                        text: {
+                            body: "Hi.. I'm VENDOR1, your forwarded message is " + msg_body
+                        }
+                    },
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+
+                });
+            }
             res.sendStatus(200);
         } else {
             res.sendStatus(404);
