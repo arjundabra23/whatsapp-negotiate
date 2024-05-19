4064138466155bbadd7f25ca93ca92e6fe1998d7
Abdullah Nauman <abdullahnauman@google.com>
2024-05-18 17:05:40 -0700

    push

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/webhook", (req, res) => {
 
 });
 
-app.post("/webhook", (req, res) => { //i want some 
+app.get("/webhook", (req, res) => { //i want some 
 
     let body_param = req.body;
 
